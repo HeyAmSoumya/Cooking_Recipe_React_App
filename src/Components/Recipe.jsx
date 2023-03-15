bf7dda05c7c00d5b3e7b36c4538cc65b3beb457e
Soumya Subhrajit Bag <soumyasubhrajit@gmail.com>
2023-03-16 02:51:53 +0530

    chnages in edit and delete button.

--- src/Components/Recipe.jsx
+++ src/Components/Recipe.jsx
@@ -1,35 +1,35 @@
-import React from 'react'
-import IngredientList from './IngredientList';
-const Recipe = ({name , servings , cookTime,instruction , ingredient}) =>{
- return(
-  <div>
-    <h3>{name}</h3>
-    <div>
-      <button>Edit</button>
-      <button>Delete</button>
-    </div>
-    <div>
-      <span>Cooking Time: </span>
-      <span>{cookTime}</span>
-    </div>
-    <div>
-      <span>Serving: </span>
-      <span>{servings}</span>
-    </div>
-    <div>
-      <span>Instruction: </span>
-      <div>
-        {instruction}
-      </div>
-    </div>
+import React from "react";
+import IngredientList from "./IngredientList";
+const Recipe = ({ name, servings, cookTime, instruction, ingredient }) => {
+  return (
+    <div className="recipe">
+      <div className="recipe__header">
+        <h3 className="recipe__title">{name}</h3>
+        <div>
+          <button className="btn btn--primary mr-1">Edit</button>
+          <button className="btn btn--danger mr-1">Delete</button>
+        </div>
+        <div>
+          <span>Cooking Time: </span>
+          <span>{cookTime}</span>
+        </div>
+        <div>
+          <span>Serving: </span>
+          <span>{servings}</span>
+        </div>
+        <div>
+          <span>Instruction: </span>
+          <div>{instruction}</div>
+        </div>
 
-    <div>
-      <span>Ingredients: </span>
-      <div>
-        <IngredientList ingredient={ingredient} /> 
+        <div>
+          <span>Ingredients: </span>
+          <div>
+            <IngredientList ingredient={ingredient} />
+          </div>
+        </div>
       </div>
     </div>
-  </div>
- )
-}
+  );
+};
 export default Recipe;
